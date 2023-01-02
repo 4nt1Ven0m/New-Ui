5a4920b7a2e4935fb53c0b525aad0ff90c33ca84
4nt1Ven0m <120436600+4nt1Ven0m@users.noreply.github.com>
2023-01-02 17:13:01 -0500

    Update CB.lua

--- CB.lua
+++ CB.lua
@@ -11,7 +11,7 @@ if getgenv().library then
     getgenv().library:Unload()
 end
 
-local library = {design = getgenv().design == "kali" and "kali" or "uwuware", tabs = {}, draggable = true, flags = {}, title = "Oblivion V1", open = false, popup = nil, instances = {}, connections = {}, options = {}, notifications = {}, tabSize = 0, theme = {}, foldername = "OblivionCFG", fileext = ".cfg"}
+local library = {design = getgenv().design == "kali" and "kali" or "uwuware", tabs = {}, draggable = true, flags = {}, title = "Oblivion V1", open = false, popup = nil, instances = {}, connections = {}, options = {}, notifications = {}, tabSize = 0, theme = {}, foldername = "OblivionCFG", fileext = ".txt"}
 getgenv().library = library
 
 --Locals
